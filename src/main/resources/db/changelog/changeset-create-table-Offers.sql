285cc6bfdb78485692478eed3fb1d937ef66248e
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2025-03-11 19:08:57 +0100

    First implementation of liquibase to the project
    Co-authored-by: s24399-pj <92204687+s24399-pj@users.noreply.github.com>

---
+++ src/main/resources/db/changelog/changeset-create-table-Offers.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+--changeset s25323:create-table-Offer
+
+CREATE TABLE offers
+(
+    id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    version         INTEGER,
+    title           VARCHAR(255),
+    description     VARCHAR(2000),
+    car_details_id  UUID UNIQUE,
+    price           NUMERIC(19, 2),
+    currency        VARCHAR(10),
+    location        VARCHAR(255),
+    contact_phone   VARCHAR(50),
+    contact_email   VARCHAR(255),
+    created_at      TIMESTAMP        DEFAULT NOW(),
+    updated_at      TIMESTAMP        DEFAULT NOW(),
+    expiration_date TIMESTAMP,
+    view_count      INTEGER,
+    featured        BOOLEAN          DEFAULT FALSE,
+    negotiable      BOOLEAN          DEFAULT TRUE,
+    CONSTRAINT fk_car_details FOREIGN KEY (car_details_id) REFERENCES car_details (id)
+);
