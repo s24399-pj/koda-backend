861a128a059fac6c2e1dbaadfe7c4f853356fbd6
s25670-pj <s25670@pjwstk.edu.pl>
2025-03-21 14:21:43 +0100

    added cors configuration

---
+++ src/main/java/pl/pjwstk/kodabackend/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package pl.pjwstk.kodabackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class CorsConfig {
+
+    @Value("${koda.frontend.url}")
+    private String frontendUrl;
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendUrl)
+                        .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                        .allowCredentials(true)
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
